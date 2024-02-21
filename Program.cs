d511aba2dad99911d0f9dc86eb88b8a2cc4d410c
simonhodne <nomis.hodne@gmail.com>
2024-02-21 15:09:03 +0100

    GREEN on sorting chars

--- Program.cs
+++ Program.cs
@@ -5,36 +5,42 @@ using Colors = AnsiTools.ANSICodes.Colors;
 
 class Program
 {
-    static async void Main(string[] args)
+    const string TEST_ARG = "-t";
+    static void Main(string[] args)
     {
-        if(args[0] == "-t")
+        if(args[0] == TEST_ARG)
         {
             Tests.RunTests();
         }
         else
         {
-            Console.Clear();
-            Console.WriteLine("Starting Assignment 2");
-
-            // SETUP 
-            const string myPersonalID = "b3b323af01cc8000db9688fac0005010ad30b396cea4c7de66d8efdc49e5b2c6"; 
-            const string baseURL = "https://mm-203-module-2-server.onrender.com/";
-            const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
-            const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
-
-            // Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
-            HttpUtils httpUtils = HttpUtils.instance;
-
-            //#### REGISTRATION
-            // We start by registering and getting the first task
-            Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
-            Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
-            string taskID = "otYK2"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
-
-            //#### FIRST TASK 
-            // Fetch the details of the task from the server.
-            Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
-            Console.WriteLine(task1Response);
+            Run();
         }
+    }
+
+    static async void Run()
+    {
+        Console.Clear();
+        Console.WriteLine("Starting Assignment 2");
+
+        // SETUP 
+        const string myPersonalID = "b3b323af01cc8000db9688fac0005010ad30b396cea4c7de66d8efdc49e5b2c6"; 
+        const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+        const string startEndpoint = "start/"; // baseURl + startEndpoint + myPersonalID
+        const string taskEndpoint = "task/";   // baseURl + taskEndpoint + myPersonalID + "/" + taskID
+
+        // Creating a variable for the HttpUtils so that we dont have to type HttpUtils.instance every time we want to use it
+        HttpUtils httpUtils = HttpUtils.instance;
+
+        //#### REGISTRATION
+        // We start by registering and getting the first task
+        Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+        Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
+        string taskID = "otYK2"; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
+
+        //#### FIRST TASK 
+        // Fetch the details of the task from the server.
+        Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID); // Get the task from the server
+        Console.WriteLine(task1Response);
     }
 }
--- StringSorter.cs
+++ StringSorter.cs
@@ -1,8 +1,100 @@
 public static class StringSorter
 {
-    public static string SortString(string stringToSort)
+    const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static string GetSortedString(string stringToSort)
     {
         string output = "";
+        string[] words = stringToSort.Split(',');
+        output = SortWords(words);
         return output;
     }
+    /*
+    public static Stack<Word> SplitString(string stringToSort)
+    {
+        Stack<Word> output = new();
+        int characterCounter = 0;
+        int previousSubstring = 0;
+        foreach(char character in stringToSort)
+        {
+            if(character == ',')
+            {
+                string subString = stringToSort.Substring(previousSubstring, characterCounter);
+                previousSubstring = characterCounter; //Check obo on this
+                subString = subString.Replace(",", "");
+                Word word = new(subString);
+            }
+            characterCounter++;
+        }
+
+        return output;
+    }
+    */
+    public static string SortWords(string[] words)
+    {
+        string output = "";
+        int wordCount = 0;
+        foreach(char letter in ALPHABET)
+        {
+            foreach(string word in words)
+            {
+                if(word.ToUpper()[0] == letter)
+                {
+                    output = output + word + ",";
+                    wordCount++;
+                }
+            }
+            if(wordCount == words.Length)
+            {
+                break;
+            }
+        }
+        output = output.Substring(0, output.Length-1);
+        Console.WriteLine(output);
+        return output;
+    }
+
+    static void TestAlphOrder()
+    {
+        
+    }
 }
+
+/*
+public class Word
+{
+    public Word(string subString)
+    {
+        SubStringWord = subString;
+    }
+    string SubStringWord {get;set;}
+}
+*/
+
+        /*int currentFirstCharIndex = 25;
+        foreach(string word in words)
+        {
+            if(output == "")
+            {
+                output = word;
+            }
+            else if(word.ToUpper()[0] == output.ToUpper()[0])
+            {
+                for(int charIndex = 1; charIndex < word.Length; charIndex++)
+                {
+                    if(word[charIndex] == output[charIndex])
+                    {
+
+                    }
+                }
+            }
+            else if(ALPHABET[..currentFirstCharIndex].Contains(word.ToUpper()[0]))
+            {
+                output = word + "," + output;
+            }
+            else
+            {
+                output = word +
+            }
+            
+        }
+        */
