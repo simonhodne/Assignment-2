519b590563b5ec5502a23da1b24dab0ecf012397
simonhodne <nomis.hodne@gmail.com>
2024-02-21 14:34:10 +0100

    Additional test added for StringSorter

--- Tests.cs
+++ Tests.cs
@@ -2,10 +2,12 @@ using static AnsiTools.ANSICodes;
 class Tests
 {
     static string inputString1 = "K,C,b,f,o,A";
+    static string[] inputStringArray1 = new string[]{"K","C","b","f","o","A"};
     static string outputString1 = "A,b,C,f,K,o";
     static public void RunTests()
     {
-        Test<string>(outputString1, StringSorter.SortString(inputString1), "Sort test single letters");
+        Test<string>(outputString1, StringSorter.SortWords(inputStringArray1), "SortWords CHAR TEST");
+        Test<string>(outputString1, StringSorter.GetSortedString(inputString1), "GetSortedString CHAR TEST");
     }
     
     static void Test<T>(T expected, T actual, string description)
