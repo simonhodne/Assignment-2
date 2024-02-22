f86cbbfb6da9a370671aa3ad7143e1492e868e3c
simonhodne <nomis.hodne@gmail.com>
2024-02-22 16:05:52 +0100

    Added a storage class for deserialized JSON info

---
+++ Deserialize.cs
@@ -0,0 +1,10 @@
+namespace Deserialize
+{
+    public class TaskInfo
+    {
+        public TaskInfo taskInfo;
+        public string title {get; set;}
+        public string description {get; set;}
+        public string parameters {get; set;}
+    }
+}
