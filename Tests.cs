bf20029d881999a3a14d8d571e40962bf4d34555
simonhodne <nomis.hodne@gmail.com>
2024-02-22 15:58:02 +0100

    Namespacing

--- Tests.cs
+++ Tests.cs
@@ -1,45 +1,49 @@
 using static AnsiTools.ANSICodes;
-class Tests
+
+namespace Test
 {
-    const string STRINGSORTER = "StringSorter";
-    static public void RunTests(string testType, bool detailsToConsole)
+    class Tests
     {
-        if(testType == STRINGSORTER)
+        const string STRINGSORTER = "StringSorter";
+        static public void RunTests(string testType, bool detailsToConsole)
         {
-            RunStringSorterTests(detailsToConsole);
+            if(testType == STRINGSORTER)
+            {
+                RunStringSorterTests(detailsToConsole);
+            }
         }
-    }
 
-    static string sortStringTestInput1 = "K,C,b,f,o,A";
-    static string sortStringTestOutput1 = "A,b,C,f,K,o";
-    static string sortStringTestInput2 = "AC,PO,Ab,KI,Aa,JL,ad";
-    static string sortStringTestOutput2 = "Aa,Ab,AC,ad,JL,KI,PO";
-    static string sortStringTestInput3 = "tullball,tull,tullete,DOMIHODE,tulleBukk,Computer,tulle,you,just,activated,my,trap,card";
-    static string sortStringTestOutput3 = "activated,card,Computer,DOMIHODE,just,my,trap,tull,tullball,tulle,tulleBukk,tullete,you";
+        static string sortStringTestInput1 = "K,C,b,f,o,A";
+        static string sortStringTestOutput1 = "A,b,C,f,K,o";
+        static string sortStringTestInput2 = "AC,PO,Ab,KI,Aa,JL,ad";
+        static string sortStringTestOutput2 = "Aa,Ab,AC,ad,JL,KI,PO";
+        static string sortStringTestInput3 = "tullball,tull,tullete,DOMIHODE,tulleBukk,Computer,tulle,you,just,activated,my,trap,card";
+        static string sortStringTestOutput3 = "activated,card,Computer,DOMIHODE,just,my,trap,tull,tullball,tulle,tulleBukk,tullete,you";
 
-    static void RunStringSorterTests(bool detailsToConsole)
-    {
-        Test<string>(sortStringTestOutput1, StringSorter.SortString(sortStringTestInput1), "SortString CHAR TEST", detailsToConsole);
-        Test<string>(sortStringTestOutput2, StringSorter.SortString(sortStringTestInput2),"SortString TWO CHAR TEST", detailsToConsole);
-        Test<string>(sortStringTestOutput3, StringSorter.SortString(sortStringTestInput3), "SortString FULL TEST", detailsToConsole);
-    }
-    
-    static void Test<T>(T expected, T actual, string description, bool detailsToConsole)
-    {
-        if(expected.Equals(actual))
+        static void RunStringSorterTests(bool detailsToConsole)
         {
-            Console.WriteLine(Colors.Green + description);
+            Test<string>(sortStringTestOutput1, StringSorter.StringSorter.SortString(sortStringTestInput1), "SortString CHAR TEST", detailsToConsole);
+            Test<string>(sortStringTestOutput2, StringSorter.StringSorter.SortString(sortStringTestInput2),"SortString TWO CHAR TEST", detailsToConsole);
+            Test<string>(sortStringTestOutput3, StringSorter.StringSorter.SortString(sortStringTestInput3), "SortString FULL TEST", detailsToConsole);
         }
-        else
+        
+        static void Test<T>(T expected, T actual, string description, bool detailsToConsole)
         {
-            Console.WriteLine(Colors.Red + description);
-        }
-        Console.Write(Colors.White);
+            if(expected.Equals(actual))
+            {
+                Console.WriteLine(Colors.Green + description);
+            }
+            else
+            {
+                Console.WriteLine(Colors.Red + description);
+            }
+            Console.Write(Colors.White);
 
-        if(detailsToConsole)
-        {
-            Console.WriteLine("Expected: " + expected);
-            Console.WriteLine("Actual: " + actual);
+            if(detailsToConsole)
+            {
+                Console.WriteLine("Expected: " + expected);
+                Console.WriteLine("Actual: " + actual);
+            }
         }
     }
 }
