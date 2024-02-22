e8ae88170cfc2acaae3b3155370365e3d8713bb5
simonhodne <nomis.hodne@gmail.com>
2024-02-23 00:00:38 +0100

    Deserialize Removed

--- Deserialize.cs
+++
@@ -1,9 +0,0 @@
-namespace Deserialize
-{
-    public class TaskInfo
-    {
-        public string title {get; set;}
-        public string description {get; set;}
-        public string parameters {get; set;}
-    }
-}
