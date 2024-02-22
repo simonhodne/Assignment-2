7fb12fbf3cf4a33e760d6e6d944b6a3a0eeea41b
simonhodne <nomis.hodne@gmail.com>
2024-02-22 15:58:41 +0100

    Namespacing & Added duplicate removal

--- StringSorter.cs
+++ StringSorter.cs
@@ -1,114 +1,148 @@
-public static class StringSorter
+namespace StringSorter
 {
-    const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static class StringSorter
+    {
+        const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static string SortString(string stringToSort)
-    {
-        string[] words = stringToSort.Split(',');
-        int longestWordLength = GetLengthOfLongestWord(words);
+        public static string SortString(string stringToSort)
+        {
+            string[] splitWords = stringToSort.Split(',');
+            string[] words = RemoveDuplicates(splitWords);
+            int longestWordLength = GetLengthOfLongestWord(words);
 
-        words = FillShortWords(words, longestWordLength);
-        words = SortWords(words, longestWordLength);
-        words = RemoveFillerFromWords(words);
+            words = FillShortWords(words, longestWordLength);
+            words = SortWords(words, longestWordLength);
+            words = RemoveFillerFromWords(words);
 
-        string output = FormatString(words);
-        return output;
-    }
-    static void Debug(string[] words)
-    {
-        foreach(string word in words)
-        {
-            Console.WriteLine(word);
+            string output = FormatString(words);
+            return output;
         }
-        Console.ReadKey();
-    }
-    public static int GetLengthOfLongestWord(string[] words)
-    {
-        int longestWordLength = 0;
-        foreach(string word in words)
-        {
-            if(word.Length > longestWordLength)
-            {
-                longestWordLength = word.Length;
-            }
-        }
-        return longestWordLength;
-    }
-    
-    public static string[] FillShortWords(string[] words, int longestWordLength)
-    {
-        string[] filledWords = new string[words.Length];
-        for(int w = 0; w < words.Length; w++)
-        {
-            while(words[w].Length < longestWordLength)
-            {
-                words[w] += ",";
-            }
-            filledWords[w] = words[w];
-        }
-        return filledWords;
-    }
 
-    public static string[] SortWords(string[] words, int longestWordLength)
-    {
-        string[] sortedWords = words;
-        
-        for(int letterIndex = longestWordLength-1; letterIndex >= 0; letterIndex--)
+        static string[] RemoveDuplicates(string[] words)
         {
-            sortedWords = SortBySelectLetter(sortedWords, letterIndex);
-        }
-        return sortedWords;
-    }
-
-    public static string[] SortBySelectLetter(string[] words, int letterIndex)
-    {
-        string[] sortedWords = new string[words.Length];
-        int sortedCount = 0;
-        for(int w = 0; w < words.Length; w++)
-        {
-            if(words[w][letterIndex] == ',')
-            {
-                sortedWords[sortedCount] = words[w];
-                sortedCount++;
-            }    
-        }
-        
-        foreach(char letter in ALPHABET)
-        {
+            int dupesFound = 0;
             for(int w = 0; w < words.Length; w++)
             {
-                    if(words[w].ToUpper()[letterIndex] == letter)
+                
+                for(int i = 0; i < words.Length; i++)
+                {
+                    if(w != i)
                     {
-                        sortedWords[sortedCount] = words[w];
-                        sortedCount++;
+                        if(words[w] == words[i])
+                        {
+                            dupesFound++;
+                        }
                     }
+                }
             }
-            if(sortedCount == words.Length)
+            dupesFound /= 2;
+            
+            string[] noDupeWords = new string[words.Length-dupesFound];
+            int wordCounter = 0;
+            for(int w = 0; w < words.Length; w++)
             {
-                break;
+                if(!noDupeWords.Contains(words[w]))
+                {
+                    noDupeWords[wordCounter] = words[w];
+                    wordCounter++;
+                }
+
+                if(wordCounter == noDupeWords.Length)
+                {
+                    break;
+                }
             }
+            return noDupeWords;
         }
-        return sortedWords;
-    }
 
-    public static string[] RemoveFillerFromWords(string[] words)
-    {
-        string[] wordsWithoutFiller = new string[words.Length];
-        for (int w = 0; w < words.Length; w++)
+        static int GetLengthOfLongestWord(string[] words)
         {
-            wordsWithoutFiller[w] = words[w].Replace(",", "");
+            int longestWordLength = 0;
+            foreach(string word in words)
+            {
+                if(word.Length > longestWordLength)
+                {
+                    longestWordLength = word.Length;
+                }
+            }
+            return longestWordLength;
         }
-        return wordsWithoutFiller;
-    }
+        
+        static string[] FillShortWords(string[] words, int longestWordLength)
+        {
+            string[] filledWords = new string[words.Length];
+            for(int w = 0; w < words.Length; w++)
+            {
+                while(words[w].Length < longestWordLength)
+                {
+                    words[w] += ",";
+                }
+                filledWords[w] = words[w];
+            }
+            return filledWords;
+        }
 
-    public static string FormatString(string[] words)
-    {
-        string output = "";
-        for(int w = 0; w < words.Length; w++)
+        static string[] SortWords(string[] words, int longestWordLength)
         {
-            output = output + words[w] + ",";
+            string[] sortedWords = words;
+            
+            for(int letterIndex = longestWordLength-1; letterIndex >= 0; letterIndex--)
+            {
+                sortedWords = SortBySelectLetter(sortedWords, letterIndex);
+            }
+            return sortedWords;
         }
-        output = output.Substring(0,output.Length-1);
-        return output;
+
+        static string[] SortBySelectLetter(string[] words, int letterIndex)
+        {
+            string[] sortedWords = new string[words.Length];
+            int sortedCount = 0;
+            for(int w = 0; w < words.Length; w++)
+            {
+                if(words[w][letterIndex] == ',')
+                {
+                    sortedWords[sortedCount] = words[w];
+                    sortedCount++;
+                }    
+            }
+            
+            foreach(char letter in ALPHABET)
+            {
+                for(int w = 0; w < words.Length; w++)
+                {
+                        if(words[w].ToUpper()[letterIndex] == letter)
+                        {
+                            sortedWords[sortedCount] = words[w];
+                            sortedCount++;
+                        }
+                }
+                if(sortedCount == words.Length)
+                {
+                    break;
+                }
+            }
+            return sortedWords;
+        }
+
+        static string[] RemoveFillerFromWords(string[] words)
+        {
+            string[] wordsWithoutFiller = new string[words.Length];
+            for (int w = 0; w < words.Length; w++)
+            {
+                wordsWithoutFiller[w] = words[w].Replace(",", "");
+            }
+            return wordsWithoutFiller;
+        }
+
+        static string FormatString(string[] words)
+        {
+            string output = "";
+            for(int w = 0; w < words.Length; w++)
+            {
+                output = output + words[w] + ",";
+            }
+            output = output.Substring(0,output.Length-1);
+            return output;
+        }
     }
 }
